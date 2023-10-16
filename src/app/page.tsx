a201bde5684572c499f9820dab119e22a7f6682d
samir01 <samir.os.abdalla@gmail.com>
2023-10-16 12:16:40 -0700

    Created overall page structure for the portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import Navbar from '@/Components/Navbar/Navbar'
+import Hero from '@/Components/Hero/Hero'
+import About from '@/Components/About/About'
+import Skills from '@/Components/Skills/Skills'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
     </main>
   )
 }
