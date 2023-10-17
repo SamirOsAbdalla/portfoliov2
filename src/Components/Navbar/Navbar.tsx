408c1aa9e24205604b72cd9a4739d0ecdb6f5ce3
samir01 <samir.os.abdalla@gmail.com>
2023-10-17 15:14:52 -0700

    Removed numbers and added refs to scroll to

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import "./Navbar.css"
 
 import { useEffect, useRef, useState } from 'react'
-export default function Navbar() {
+
+
+interface Props {
+    aboutRef: any,
+    skillsRef: any,
+    projectsRef: any,
+}
+
+type navScrollSection = "home" | "about" | "skills" | "projects"
+export default function Navbar({ aboutRef, skillsRef, projectsRef }: Props) {
     const setNavbarActive = () => {
         const mainNav = document.querySelector(".main__nav")
         mainNav?.classList.toggle("active")
@@ -33,22 +42,79 @@ export default function Navbar() {
         });
     }, [])
 
+    const handleNavClick = (navScrollSection: navScrollSection) => {
+
+        switch (navScrollSection) {
+            case ("home"): {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                });
+                break;
+            }
+            case ("about"): {
+                aboutRef.current?.scrollIntoView({ behavior: "smooth" })
+                break;
+            }
+
+            case ("skills"): {
+                skillsRef.current?.scrollIntoView({ behavior: "smooth" })
+                break;
+            }
+            case ("projects"): {
+                projectsRef.current?.scrollIntoView({ behavior: "smooth" })
+                break;
+            }
+        }
+    }
+
+
+    //mostly repeated code (could maybe move switch into seperate function)
+    //small function so maybe ok
+    const handleDropdownClick = (navScrollSection: navScrollSection) => {
+        const mainNav = document.querySelector(".main__nav")
+        const navHeader = document.querySelector(".nav__header")
+
+        switch (navScrollSection) {
+            case ("home"): {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                });
+                break;
+            }
+            case ("about"): {
+                aboutRef.current?.scrollIntoView({ behavior: "smooth" })
+                break;
+            }
+
+            case ("skills"): {
+                skillsRef.current?.scrollIntoView({ behavior: "smooth" })
+                break;
+            }
+            case ("projects"): {
+                projectsRef.current?.scrollIntoView({ behavior: "smooth" })
+                break;
+            }
+        }
+
+        mainNav?.classList.toggle("active")
+
+        navHeader?.classList.toggle("active")
+    }
     return (
         <header className={`nav__header ${navHiddenClass == "nav__hidden" ? "nav__hidden" : ""}`}>
             <div className="main__nav">
-                <div className="name__container"> &lt;<span className="name__span">Samir </span>/&gt;</div>
+                <div onClick={() => handleNavClick("home")} className="name__container"> &lt;<span className="name__span">Samir </span>/&gt;</div>
                 <nav>
                     <ul>
-                        <li>
-                            <span className="nav__number">01.</span>
+                        <li onClick={() => handleNavClick("about")}>
                             <span className="nav__text">About</span>
                         </li>
-                        <li>
-                            <span className="nav__number">02.</span>
+                        <li onClick={() => handleNavClick("skills")}>
                             <span className="nav__text">Skills</span>
                         </li>
-                        <li>
-                            <span className="nav__number">03.</span>
+                        <li onClick={() => handleNavClick("projects")}>
                             <span className="nav__text">Projects</span>
                         </li>
 
@@ -60,16 +126,13 @@ export default function Navbar() {
             </div>
             <section className="dropdown__menu">
                 <ul>
-                    <li>
-                        <span className="nav__number">01.</span>
+                    <li onClick={() => handleDropdownClick("about")}>
                         <span className="nav__text">About</span>
                     </li>
-                    <li>
-                        <span className="nav__number">02.</span>
+                    <li onClick={() => handleDropdownClick("skills")}>
                         <span className="nav__text">Skills</span>
                     </li>
-                    <li>
-                        <span className="nav__number">03.</span>
+                    <li onClick={() => handleDropdownClick("projects")}>
                         <span className="nav__text">Projects</span>
                     </li>
 
