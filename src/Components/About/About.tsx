f29472d2abd53f610dd3c4a1a4e19e9bb32cefe6
samir01 <samir.os.abdalla@gmail.com>
2023-10-17 15:14:33 -0700

    Added refs and on load animation

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
 import "./About.css"
+import { forwardRef } from 'react'
+import { motion } from "framer-motion"
 
-export default function About() {
+function About(props: any, ref: any) {
     return (
-        <section className="about__wrapper">
-            <div className="about__text">
-                <h2 className="section__header about__header">01. <span>About</span></h2>
-                <p> I am a developer with a bachelor&apos;s in Mathematics of Computation from
-                    UCLA. Seeing as how I like the &quot;computation&quot; side much more than the &quot;mathematics&quot;, I have found a great joy
-                    in creating software and showing off a completed product. <br /><br />My specialty is
-                    with the <span>React</span> and <span>Typescript</span> ecosystem, but I am
-                    constantly learning new technologies to try and stay up-to-date with the ever-changing industry standards.
-                    Currently, I am looking for work and hoping to grow and develop my skills with a company.
-                    When I am not working on anything, I enjoy playing basketball with friends, gaming, and thinking
-                    about that one bug that popped up in my last coding session (it happens every single time).
-                </p>
-                <div className="about__text__bold">Listed below are the main technologies I use</div>
-                <ul className="about__list">
-                    <li><span>React</span></li>
-                    <li><span>Typescript</span></li>
-                    <li><span>MongoDB</span></li>
-                    <li><span>NodeJS/Express</span></li>
-                </ul>
-            </div>
+
+        <section ref={ref} className="about__wrapper">
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: .6, ease: "easeIn" }}
+                viewport={{ once: true, amount: 0.1 }}
+            >
+                <div className="about__text">
+                    <h2 className="section__header about__header"><span>About</span></h2>
+                    <p> I am a developer with a bachelor&apos;s in Mathematics of Computation from
+                        UCLA. Seeing as how I like the &quot;computation&quot; side much more than the &quot;mathematics&quot;, I have found a great joy
+                        in creating software and showing off a completed product. <br /><br />My specialty is
+                        with the <span>React</span> and <span>Typescript</span> ecosystem, but I am
+                        constantly learning new technologies to try and stay up-to-date with the ever-changing industry standards.
+                        Currently, I am looking for work and hoping to grow and develop my skills with a company.
+                        When I am not working on anything, I enjoy playing basketball with friends, gaming, and thinking
+                        about that one bug that popped up in my last coding session (it happens every single time).
+                    </p>
+                    <div className="about__text__bold">Listed below are the main technologies I use</div>
+                    <ul className="about__list">
+                        <li><span>React</span></li>
+                        <li><span>Typescript</span></li>
+                        <li><span>MongoDB</span></li>
+                        <li><span>NodeJS/Express</span></li>
+                    </ul>
+                </div>
+            </motion.div >
         </section>
+
+
     )
 }
+
+export default forwardRef(About)
--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -8,99 +8,147 @@ import nodeJSLogo from "../../../public/nodejs.svg"
 import expressLogo from "../../../public/expressjs.svg"
 import cppLogo from "../../../public/c.svg"
 import gitLogo from "../../../public/git.svg"
+import nextJSLogo from "../../../public/nextjs.svg"
+import reactLogo from "../../../public/react.svg"
+import { motion } from "framer-motion"
+
+import { forwardRef } from 'react'
 import Image from 'next/image'
 
-export default function Skills() {
+const skillsArray = ["HTML", "CSS", "JavaScript", "Typescript", "React", "NextJS", "NodeJS"
+    , "ExpressJS", "MongoDB", "Redux", "Git"]
+function Skills(props: any, ref: any) {
     return (
-        <section className="skills__wrapper">
-            <h2 className="section__header skills__header">02. <span>Skills</span></h2>
-            <p className="skills__text">
-                My web technologies are part of what is commonly known as the MERN stack. In addition to this,
-                I work with C++ for pet projects and for simply deepening my programming
-                knowledge. I have highlighted below the technical skills that I will bring
-                to your project.
-            </p>
-            <div className="skills__technologies">
-                <div className="skills__card">
-                    <div className="skills__image__container">
-                        <div className="skills__image image__first">
-                            <Image width={50} height={50} alt="css" src={cssLogo} />
+
+        <section ref={ref} className="skills__wrapper">
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: .6, ease: "easeIn" }}
+                viewport={{ once: true, amount: 0.1 }}
+                className="skill__motion"
+            >
+                <h2 className="section__header skills__header"><span>Skills</span></h2>
+                <p className="skills__text">
+                    My web technologies are part of what is commonly known as the MERN stack. I have highlighted below the technical skills that I will bring
+                    to your project. At the bottom is an overall summary of what I use for web development.
+                </p>
+                <div className="skills__technologies__wrapper">
+                    <div className="skills__technologies">
+                        <div className="skills__card">
+                            <div className="skills__image__container">
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={nextJSLogo} />
+                                </div>
+                            </div>
+                            <div className="skills__card__title">
+                                Full-Stack Development
+                            </div>
+                            <p className="skills__paragraph">
+                                My experience in web development spans both the frontend and the backend.
+                                I use <span className="skills__bold">NextJS</span> for not only the ease of use when it comes to handling API
+                                routes, but for its many optimizations and quality of life changes
+                                with images, SSR, etc.
+                            </p>
                         </div>
-                        <div className="skills__image">
-                            <Image width={50} height={50} alt="css" src={htmlLogo} />
+                        <div className="skills__card">
+                            <div className="skills__image__container">
+                                <div className="skills__image image__first">
+                                    <Image width={50} height={50} alt="css" src={cssLogo} />
+                                </div>
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={htmlLogo} />
+                                </div>
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={typescriptLogo} />
+                                </div>
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={reactLogo} />
+                                </div>
+                            </div>
+                            <div className="skills__card__title">
+                                Frontend Development
+                            </div>
+                            <p className="skills__paragraph">
+                                My frontend development stack is comprised of good ol&apos; <span className="skills__bold">HTML</span>, <span className="skills__bold">CSS</span>, <span className="skills__bold">Typescript</span>, and <span className="skills__bold">React</span>. I use Typescript instead of Javascript because I prefer
+                                working with data that is explicitly typed which prevents a lot of headaches.
+                                I take pride in building clean and functional websites
+                                that provide users with a positive experience.
+                            </p>
                         </div>
-                        <div className="skills__image image__third">
-                            <Image width={50} height={50} alt="css" src={typescriptLogo} />
+                        <div className="skills__card ">
+                            <div className="skills__image__container skills__image__nodeexpress">
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={nodeJSLogo} />
+                                </div>
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={expressLogo} />
+                                </div>
+                            </div>
+                            <div className="skills__card__title">
+                                Backend Development
+                            </div>
+                            <p className="skills__paragraph">
+                                My backend experience has come from working with <span className="skills__bold">NodeJS</span> and <span className="skills__bold">ExpressJS</span>.
+                                Working with many different projects has allowed me to
+                                interact with API&apos;s, set up routes, handle middleware and errors,
+                                and much more.
+                            </p>
                         </div>
-                    </div>
-                    <div className="skills__card__title">
-                        Frontend Development
-                    </div>
-                    <p className="skills__paragraph">
-                        My frontend development stack is comprised of good ol&apos; HTML, CSS,
-                        and Typescript. I use Typescript instead of Javascript because I prefer
-                        working with data that is explicitly typed which prevents a lot of headaches.
-                        I take pride in building clean and functional websites
-                        that provide users with a positive experience.
-                    </p>
-                </div>
-                <div className="skills__card">
-                    <div className="skills__image__container">
-                        <div className="skills__image">
-                            <Image width={50} height={50} alt="css" src={mongoDBLogo} />
+                        <div className="skills__card">
+                            <div className="skills__image__container">
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={mongoDBLogo} />
+                                </div>
+
+                            </div>
+                            <div className="skills__card__title">
+                                Database Development
+                            </div>
+                            <p className="skills__paragraph">
+                                I have extensive experience with working with <span className="skills__bold">MongoDB</span> seeing as it has
+                                been the main database used in my projects. From simple CRUD operations to
+                                more complicated queries, I can perform any type of data manipulation
+                                needed for the project at hand.
+                            </p>
                         </div>
 
+                        <div className="skills__card ">
+                            <div className="skills__image__container">
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={cppLogo} />
+                                </div>
+                                <div className="skills__image">
+                                    <Image width={50} height={50} alt="css" src={gitLogo} />
+                                </div>
+                            </div>
+                            <div className="skills__card__title">
+                                Misc.
+                            </div>
+                            <p className="skills__paragraph">
+                                I am very familiar with <span className="skills__bold">C++</span> and enjoy using it to build video games with the SFML library.
+                                C++ has also helped greatly in maintaining and reinforcing my computer science fundamentals especially
+                                when it comes to OOP. Version control is also no stranger and I employ <span className="skills__bold">git</span> in each one of my projects
+                                to preserve a clear history of my work.
+
+                            </p>
+                        </div>
                     </div>
-                    <div className="skills__card__title">
-                        Database Development
-                    </div>
-                    <p className="skills__paragraph">
-                        I have extensive experience with working with MongoDB seeing as it has
-                        been the main database used in my projects. From simple CRUD operations to
-                        more complicated queries, I can perform any type of data manipulation
-                        needed for the project at hand.
-                    </p>
                 </div>
-                <div className="skills__card ">
-                    <div className="skills__image__container skills__image__nodeexpress">
-                        <div className="skills__image">
-                            <Image width={50} height={50} alt="css" src={nodeJSLogo} />
-                        </div>
-                        <div className="skills__image">
-                            <Image width={50} height={50} alt="css" src={expressLogo} />
-                        </div>
-                    </div>
-                    <div className="skills__card__title">
-                        Backend Development
-                    </div>
-                    <p className="skills__paragraph">
-                        My backend experience has come from working with NodeJS and ExpressJS.
-                        Working with many different projects has allowed me to
-                        interact with API&apos;s, set up routes, handle middleware and errors,
-                        and much more.
-                    </p>
+                <div className="skills__summary">
+                    {skillsArray.map(skill => {
+                        return (
+                            <div key={skill} className="projectitem__technology">
+                                {skill}
+                            </div>
+                        )
+                    })}
                 </div>
-                <div className="skills__card ">
-                    <div className="skills__image__container">
-                        <div className="skills__image">
-                            <Image width={50} height={50} alt="css" src={cppLogo} />
-                        </div>
-                        <div className="skills__image">
-                            <Image width={50} height={50} alt="css" src={gitLogo} />
-                        </div>
-                    </div>
-                    <div className="skills__card__title">
-                        Misc.
-                    </div>
-                    <p className="skills__paragraph">
-                        I am very familiar with C++ and enjoy using it to build video games with the SFML library.
-                        C++ has also helped greatly in maintaining and reinforcing my computer science fundamentals.
-                        Version control is also no stranger and I employ git in each one of my projects to ensure
-                        that my work is not only saved but that there is a clear history and order to the work done
-                        in each project.
-                    </p>
-                </div>
-            </div>
+            </motion.div>
+
         </section>
+
     )
 }
+
+export default forwardRef(Skills)
