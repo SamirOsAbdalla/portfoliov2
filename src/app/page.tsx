9accbd48ed0c8c69e541ce382681a0aa8b96dfba
samir01 <samir.os.abdalla@gmail.com>
2023-10-17 15:13:32 -0700

    Created refs for each section so navbar can scroll to them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,28 @@ import Navbar from '@/Components/Navbar/Navbar'
 import Hero from '@/Components/Hero/Hero'
 import About from '@/Components/About/About'
 import Skills from '@/Components/Skills/Skills'
+import Projects from '@/Components/Projects/Projects'
+import Footer from '@/Components/Footer/Footer'
 
+import { useRef } from 'react'
 export default function Home() {
+
+  const aboutRef = useRef(null)
+  const skillsRef = useRef(null)
+  const projectsRef = useRef(null)
+
   return (
     <main className={styles.main}>
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
+      <Navbar
+        aboutRef={aboutRef}
+        skillsRef={skillsRef}
+        projectsRef={projectsRef}
+      />
+      <Hero projectsRef={projectsRef} />
+      <About ref={aboutRef} />
+      <Skills ref={skillsRef} />
+      <Projects ref={projectsRef} />
+      <Footer />
     </main>
   )
 }
