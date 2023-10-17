20574ebe41dab78d7b5deaaf38e346eb3fd7f8af
samir01 <samir.os.abdalla@gmail.com>
2023-10-17 16:49:19 -0700

    Added picture to about change

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import "./About.css"
 import { forwardRef } from 'react'
 import { motion } from "framer-motion"
-
+import Image from 'next/image'
+import headshot from "../../../public/headshot.jpg"
 function About(props: any, ref: any) {
     return (
 
@@ -12,26 +13,33 @@ function About(props: any, ref: any) {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: .6, ease: "easeIn" }}
                 viewport={{ once: true, amount: 0.1 }}
+                className="about__container"
             >
-                <div className="about__text">
-                    <h2 className="section__header about__header"><span>About</span></h2>
-                    <p> I am a developer with a bachelor&apos;s in Mathematics of Computation from
-                        UCLA. Seeing as how I like the &quot;computation&quot; side much more than the &quot;mathematics&quot;, I have found a great joy
-                        in creating software and showing off a completed product. <br /><br />My specialty is
-                        with the <span>React</span> and <span>Typescript</span> ecosystem, but I am
-                        constantly learning new technologies to try and stay up-to-date with the ever-changing industry standards.
-                        Currently, I am looking for work and hoping to grow and develop my skills with a company.
-                        When I am not working on anything, I enjoy playing basketball with friends, gaming, and thinking
-                        about that one bug that popped up in my last coding session (it happens every single time).
-                    </p>
-                    <div className="about__text__bold">Listed below are the main technologies I use</div>
-                    <ul className="about__list">
-                        <li><span>React</span></li>
-                        <li><span>Typescript</span></li>
-                        <li><span>MongoDB</span></li>
-                        <li><span>NodeJS/Express</span></li>
-                    </ul>
+                <h2 className="section__header about__header"><span>About</span></h2>
+                <div className="about__main">
+                    <div className="about__text">
+                        <p> I am a developer with a bachelor&apos;s in Mathematics of Computation from
+                            UCLA. Seeing as how I like the &quot;computation&quot; side much more than the &quot;mathematics&quot;, I have found a great joy
+                            in creating software and showing off a completed product. <br /><br />My specialty is
+                            with the <span>React</span> and <span>Typescript</span> ecosystem, but I am
+                            constantly learning new technologies to try and stay up-to-date with the ever-changing industry standards.
+                            Currently, I am looking for work and hoping to grow and develop my skills with a company.
+                            When I am not working on anything, I enjoy playing basketball with friends, gaming, and thinking
+                            about that one bug that popped up in my last coding session (it happens every single time).
+                        </p>
+                        <div className="about__text__bold">Listed below are the main technologies I use</div>
+                        <ul className="about__list">
+                            <li><span>React</span></li>
+                            <li><span>Typescript</span></li>
+                            <li><span>MongoDB</span></li>
+                            <li><span>NodeJS/Express</span></li>
+                        </ul>
+                    </div>
+                    <div className="headshot__container">
+                        <Image style={{ width: '100%', height: 'auto' }} alt="Headshot" src={headshot} />
+                    </div>
                 </div>
+
             </motion.div >
         </section>
 
