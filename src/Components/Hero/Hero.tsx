b61c5292500e79334f1001e51c35087cd734b712
samir01 <samir.os.abdalla@gmail.com>
2023-10-17 15:16:10 -0700

    Added call to action and resume in case someone wants to print out and view

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -5,8 +5,16 @@ import cloudSvg from "../../../public/cloud.svg"
 import Image from 'next/image'
 import gitIcon from "../../../public/github-icon.svg"
 import linkedINIcon from "../../../public/linked-logo.svg"
+import { motion } from "framer-motion"
 
-export default function Hero() {
+interface Props {
+    projectsRef: any;
+}
+export default function Hero({ projectsRef }: Props) {
+
+    const handleViewClick = () => {
+        projectsRef?.current.scrollIntoView({ behavior: "smooth" })
+    }
     return (
         <div className="hero__wrapper">
             <section>
@@ -81,17 +89,17 @@ export default function Hero() {
                             <Image fill alt="Cloud" src={cloudSvg} />
                         </div>
                     </div>
-                    <div style={{ top: "27vh", left: "-20vw" }} className="cloud__wrapper .moveAnimationFour">
+                    <div style={{ top: "27vh", left: "-10vw" }} className="cloud__wrapper .moveAnimationFour">
                         <div style={{ width: "50px", height: "50px", opacity: "0.3" }} className="cloud">
                             <Image fill alt="Cloud" src={cloudSvg} />
                         </div>
                     </div>
-                    <div style={{ top: "47vh", left: "-10vw" }} className="cloud__wrapper moveAnimationThree">
+                    <div style={{ top: "47vh", left: "-5vw" }} className="cloud__wrapper moveAnimationThree">
                         <div style={{ width: "30px", height: "30px", opacity: "0.9" }} className="cloud">
                             <Image fill alt="Cloud" src={cloudSvg} />
                         </div>
                     </div>
-                    <div style={{ top: "67vh", left: "-20vw" }} className="cloud__wrapper .moveAnimationFive ">
+                    <div style={{ top: "67vh", left: "-10vw" }} className="cloud__wrapper .moveAnimationFive ">
                         <div style={{ width: "70px", height: "70px", opacity: "1" }} className="cloud">
                             <Image fill alt="Cloud" src={cloudSvg} />
                         </div>
@@ -111,29 +119,35 @@ export default function Hero() {
                             <Image fill alt="Cloud" src={cloudSvg} />
                         </div>
                     </div>
-                    <div className="hero__text">
-                        <div className="hero__greeting">Hi, my name is</div>
-                        <div className="hero__personalname">Samir Abdalla</div>
-                        <div className="hero__tag">I build web applications</div>
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: .6, ease: "easeIn" }}
+                        viewport={{ once: true, amount: 0.1 }}
+                    >
+                        <div className="hero__text">
+                            <div className="hero__greeting">Hi, my name is</div>
+                            <div className="hero__personalname">Samir Abdalla</div>
+                            <div className="hero__tag">I build web applications</div>
 
-                        <p className="hero__blurb">
-                            I am a Full-Stack developer who loves working with all kinds
-                            of software. I carefully break down problems in order to turn
-                            great ideas into an even better reality.
-                        </p>
-                        <div className="icon__logos">
-                            <div className="icon__container github__icon">
-                                <a href="https://github.com/SamirOsAbdalla" target="_blank" className="icon__link">
-                                    <Image fill alt="git" src={gitIcon} />
+                            <p className="hero__blurb">
+                                I am a Full-Stack developer who loves working with all kinds
+                                of software. I carefully break down problems in order to turn
+                                great ideas into an even better reality.
+                            </p>
+                            <div className="icon__logos">
+                                <div onClick={handleViewClick} className="hero__button">
+                                    View Work
+                                </div>
+                                <a href="https://samirosabdalla.github.io/Resume/Resume.pdf" target="_blank" className="hero__button">
+                                    Resume
                                 </a>
-                            </div>
-                            <div className="icon__container linkedin__icon">
-                                <a href="https://www.linkedin.com/in/samir-abdalla-402510232" target="_blank" className="icon__link">
-                                    <Image fill alt="linkedin" src={linkedINIcon} />
-                                </a>
+
                             </div>
                         </div>
-                    </div>
+
+                    </motion.div>
+
                 </div>
             </section>
         </div>
